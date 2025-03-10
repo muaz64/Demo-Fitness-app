41f9c479b904e486d4d4fe43289aa1f478e2874c
muaz64 <muazmuhammad21@gmail.com>
2025-03-10 12:36:26 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <h1>Fitness app</h1>
+      <div className="card">
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
